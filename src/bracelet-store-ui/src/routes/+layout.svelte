1f9b6ed08ea57cce516a14069fa1eaad8275d676
GitHubDanya <daniel.4ervinsky@gmail.com>
2026-09-11 21:12:46 +0300

    Add landing page

--- src/bracelet-store-ui/src/routes/+layout.svelte
+++ src/bracelet-store-ui/src/routes/+layout.svelte
@@ -5,7 +5,66 @@
 </script>
 
 <svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=PT+Serif+Caption:ital@0;1&display=swap" rel="stylesheet">
+	
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<header class="top-bar">
+	<div class="logo">
+		<a href="/">Logo</a>
+	</div>
+	<nav>
+		<a href="/store">Bracelets</a>
+		<a href="/about">About</a>
+	</nav>
+</header>
+
+<main class="container">
+	{@render children()}
+</main>
+
+<style>
+	:global(body) {
+		font-family: "PT Serif Caption", serif;
+		background-color: #ffffff;
+	}
+	
+	.top-bar {
+		display: flex;
+		justify-content: left;
+		align-items: center;
+		padding: 1rem 2rem;
+		background-color: #ffffff;
+		color: #111111;
+	}
+	
+	@media(max-width: 1024px) {
+		.top-bar {
+			justify-content: center;
+		}
+	}
+
+	.logo a {
+		font-weight: bold;
+		font-size: 1.25rem;
+		color: #111111;
+		text-decoration: none;
+	}
+
+	nav a {
+		color: #111111;
+		margin-left: 1.5rem;
+		text-decoration: none;
+	}
+
+	nav a:hover {
+		color: #ffffff;
+	}
+
+	.container {
+		padding: 2rem;
+	}
+</style>
--- src/bracelet-store-ui/src/routes/+page.svelte
+++ src/bracelet-store-ui/src/routes/+page.svelte
@@ -1,2 +1,153 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+</script>
+
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Devanagari:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
+</svelte:head>
+
+<div class="title">
+    Elegant, handmade accessories
+</div>
+
+<p class="store-row-title">Bracelets</p>
+<div class="store-row">
+    <div class="item">
+        <img class="thumbnail" src="https://le-bijouparis.com/cdn/shop/files/A65FC327-6EE9-4B58-8954-3984ED1A802E.jpg?v=1769290043&width=1206"/>
+        <h2>Silk Bracelet</h2>
+        <p class="description">Red Amethyst</p>
+    </div>
+    <div class="item">
+        <img class="thumbnail" src="https://le-bijouparis.com/cdn/shop/files/A65FC327-6EE9-4B58-8954-3984ED1A802E.jpg?v=1769290043&width=1206"/>
+        <h2>Very Complicated Fancy Bracelet</h2>
+        <p class="description">Rose Quartz, Amethyst, Lava Stone, Glass</p>
+    </div>
+    <div class="item">
+        <img class="thumbnail" src="https://le-bijouparis.com/cdn/shop/files/A65FC327-6EE9-4B58-8954-3984ED1A802E.jpg?v=1769290043&width=1206"/>
+        <h2>Simple Bracelet</h2>
+        <p class="description">String</p>
+    </div>
+</div>
+
+<p class="store-row-title">New Items</p>
+<div class="store-row">
+    <div class="item">
+        <img class="thumbnail" src="https://le-bijouparis.com/cdn/shop/files/A65FC327-6EE9-4B58-8954-3984ED1A802E.jpg?v=1769290043&width=1206"/>
+        <h2>Silk Bracelet</h2>
+        <p class="description">Red Amethyst</p>
+    </div>
+    <div class="item">
+        <img class="thumbnail" src="https://le-bijouparis.com/cdn/shop/files/A65FC327-6EE9-4B58-8954-3984ED1A802E.jpg?v=1769290043&width=1206"/>
+        <h2>Very Complicated Fancy Bracelet</h2>
+        <p class="description">Rose Quartz, Amethyst, Lava Stone, Glass</p>
+    </div>
+    <div class="item">
+        <img class="thumbnail" src="https://le-bijouparis.com/cdn/shop/files/A65FC327-6EE9-4B58-8954-3984ED1A802E.jpg?v=1769290043&width=1206"/>
+        <h2>Simple Bracelet</h2>
+        <p class="description">String</p>
+    </div>
+    <div class="item">
+        <img class="thumbnail" src="https://le-bijouparis.com/cdn/shop/files/A65FC327-6EE9-4B58-8954-3984ED1A802E.jpg?v=1769290043&width=1206"/>
+        <h2>Interesting Bracelet</h2>
+        <p class="description">Secrets</p>
+    </div>
+</div>
+
+<div class="tail">
+    © 2026
+</div>
+
+<style>
+    .title {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-weight: 200;
+        font-size: 3rem;
+        text-align: center;
+        height: calc(100vh - 100px);
+    }
+    
+    @media(min-width: 1024px) {
+        .title {
+            margin: 300px 0;
+            height: auto;
+        }
+    }
+    
+    .store-row-title {
+        margin-top: 10vh;
+    }
+    
+    .store-row {
+        display: flex;
+        overflow-x: auto;
+        padding: 0 0 15px;
+        gap: 10px;
+    }
+    
+    @media(max-width: 1024px) {
+        .store-row {
+            display: flex;
+            flex-wrap: nowrap;
+        }
+    }
+    
+    .store-row .item {
+        display: flex;
+        flex: 0 0 150px;
+        flex-direction: column;
+        border: #808080 1px solid;
+        justify-content: start;
+        align-items: center;
+        padding: 10px;
+        width: 150px;
+    }
+    
+    @media(min-width: 1024px) {
+        .store-row .item {
+            flex: 0 0 300px;
+            width: 300px;
+        }
+    }
+    
+    .store-row .item h2 {
+        font-size: 0.9rem;
+        font-weight: 600;
+        margin-top: 1rem;
+        text-align: left;
+        align-self: start;
+    }
+    
+    .store-row .item .thumbnail,
+    .store-row .item .thumbnail img {
+        width: 100%;
+        max-width: 100%;
+        height: auto;
+        display: block;
+        object-fit: cover;
+    }
+    
+    .store-row .item .description {
+        font-size: 0.6rem;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        align-self: start;
+        margin-top: 0;
+        width: 100%;
+        color: #696969;
+    }
+    
+    .tail {
+        height: 10vh;
+        margin: 25vh 0 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        color: #696969;
+        font-size: 0.6rem;
+    }
+</style>
