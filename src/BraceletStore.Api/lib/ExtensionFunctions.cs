09e99d0c61e5cdd9c0f1a2f9f340a4ab4775ee72
GitHubDanya <daniel.4ervinsky@gmail.com>
2026-09-12 00:23:55 +0300

    Create getlocalized extension function for localizedrecord

---
+++ src/BraceletStore.Api/lib/ExtensionFunctions.cs
@@ -0,0 +1,14 @@
+using BraceletStore.Api.Models;
+
+namespace BraceletStore.Api.lib;
+
+public static class ExtensionFunctions
+{
+    public static string GetLocalized(this LocalizedRecord name, Language language) =>
+        language switch
+        {
+            Language.Hebrew => name.He,
+            Language.Russian => name.Ru,
+            _ => name.En
+        };
+}
