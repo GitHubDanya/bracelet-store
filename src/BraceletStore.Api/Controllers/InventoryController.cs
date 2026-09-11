2a6d8a85d7ae275104d8c8638190e06c7d9484c6
GitHubDanya <daniel.4ervinsky@gmail.com>
2026-09-12 00:31:08 +0300

    Write mock inventory controller

---
+++ src/BraceletStore.Api/Controllers/InventoryController.cs
@@ -0,0 +1,51 @@
+using BraceletStore.Api.Models;
+using BraceletStore.Api.Models.Bracelet;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BraceletStore.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class InventoryController : ControllerBase
+{
+    private static readonly List<Bracelet> Bracelets = new()
+    {
+        new Bracelet
+        {
+            Id = 1,
+            Name = new LocalizedRecord("Lorem", "Lorem", "Lorem"),
+            Description = new LocalizedRecord("Lorem", "Lorem", "Lorem"),
+            Materials = new List<LocalizedRecord>() { new LocalizedRecord("Lorem", "Lorem", "Lorem"), },
+            Color = new LocalizedRecord("Lorem", "Lorem", "Lorem"),
+        },
+        new Bracelet
+        {
+            Id = 2,
+            Name = new LocalizedRecord("Lorem", "Lorem", "Lorem"),
+            Description = new LocalizedRecord("Lorem", "Lorem", "Lorem"),
+            Materials = new List<LocalizedRecord>() { new LocalizedRecord("Lorem", "Lorem", "Lorem"), },
+            Color = new LocalizedRecord("Lorem", "Lorem", "Lorem"),
+        },
+        new Bracelet
+        {
+            Id = 3,
+            Name = new LocalizedRecord("Lorem", "Lorem", "Lorem"),
+            Description = new LocalizedRecord("Lorem", "Lorem", "Lorem"),
+            Materials = new List<LocalizedRecord>() { new LocalizedRecord("Lorem", "Lorem", "Lorem"), },
+            Color = new LocalizedRecord("Lorem", "Lorem", "Lorem"),
+        }
+    };
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Bracelet>> GetAll()
+    {
+        return Ok(Bracelets);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Bracelet> GetById(int id)
+    {
+        Bracelet? bracelet = Bracelets.FirstOrDefault(b => b.Id == id);
+        return bracelet is not null ? Ok(bracelet) : NotFound();
+    }
+}
