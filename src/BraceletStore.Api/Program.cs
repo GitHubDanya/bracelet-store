6ea8d163703f2e783b279f48fd0fb621cbdc0fb0
GitHubDanya <daniel.4ervinsky@gmail.com>
2026-09-12 09:51:52 +0300

    Add .env functionality for api

--- src/BraceletStore.Api/Program.cs
+++ src/BraceletStore.Api/Program.cs
@@ -1,3 +1,10 @@
+using dotenv.net;
+
+DotEnv.Load(options: new DotEnvOptions(
+    probeForEnv: true,
+    probeLevelsToSearch: 4
+));
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
