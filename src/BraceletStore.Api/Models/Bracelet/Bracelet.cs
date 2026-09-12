999c22d12685a3907e326ad74bbbf326e3ae2f81
GitHubDanya <daniel.4ervinsky@gmail.com>
2026-09-12 18:03:38 +0300

    Add price entry for Bracelet model

--- src/BraceletStore.Api/Models/Bracelet/Bracelet.cs
+++ src/BraceletStore.Api/Models/Bracelet/Bracelet.cs
@@ -4,7 +4,8 @@ public class Bracelet
 {
     public required int Id { get; set; }
     public required bool Available { get; set; }
-    public required List<string> ThumbnailUrls { get; set; }
+    public required decimal Price { get; set; }
+    public required string[] ThumbnailUrls { get; set; }
     public required LocalizedRecord Name { get; set; }
     public required LocalizedRecord Description { get; set; }
     public required List<LocalizedRecord> Materials { get; set; }
