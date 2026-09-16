67c1d3e554aa875d83df4afe788e22d709ae75ed
GitHubDanya <daniel.4ervinsky@gmail.com>
2026-09-16 09:10:19 +0300

    Add FetchFilters function

--- src/BraceletStore.Api/Database/Repositories/BraceletRepository.cs
+++ src/BraceletStore.Api/Database/Repositories/BraceletRepository.cs
@@ -1,6 +1,7 @@
 using BraceletStore.Api.lib.Queries;
 using BraceletStore.Api.Models.Bracelet;
 using Dapper;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace BraceletStore.Api.Database.Repositories;
 
@@ -32,33 +33,33 @@ public static class BraceletRepository
         var builder = new SqlBuilder();
 
         var selector = builder.AddTemplate("""
-                           SELECT
-                               id AS Id, 
-                               available AS Available, 
-                               price AS Price,
-                               thumbnail_urls AS ThumbnailUrls, 
-                               name AS Name, 
-                               description AS Description, 
-                               materials AS Materials,
-                               color AS Color
-                           FROM bracelets
-                           /**where**/
-                           ORDER BY id DESC
-                           LIMIT @Limit OFFSET @Offset
-                           """,
+                                           SELECT
+                                               id AS Id, 
+                                               available AS Available, 
+                                               price AS Price,
+                                               thumbnail_urls AS ThumbnailUrls, 
+                                               name AS Name, 
+                                               description AS Description, 
+                                               materials AS Materials,
+                                               color AS Color
+                                           FROM bracelets
+                                           /**where**/
+                                           ORDER BY id DESC
+                                           LIMIT @Limit OFFSET @Offset
+                                           """,
             new
             {
                 Limit = filter.PageSize,
                 Offset = (filter.Page - 1) * filter.PageSize
             });
 
-        if (filter.AvailableOnly == true) 
+        if (filter.AvailableOnly == true)
             builder.Where("available = true");
 
-        if (filter.MinPrice.HasValue) 
+        if (filter.MinPrice.HasValue)
             builder.Where("price >= @MinPrice", new { MinPrice = filter.MinPrice.Value });
 
-        if (filter.MaxPrice.HasValue) 
+        if (filter.MaxPrice.HasValue)
             builder.Where("price <= @MaxPrice", new { MaxPrice = filter.MaxPrice.Value });
 
         if (!string.IsNullOrWhiteSpace(filter.SearchTerm))
@@ -76,6 +77,28 @@ public static class BraceletRepository
         return await DataProvider.QueryManyAsync(db =>
             db.QueryAsync<Bracelet>(selector.RawSql, selector.Parameters));
     }
+
+    public static async Task<QueryResult<List<string>>> FetchMaterials(BraceletFilter? filter = null)
+    {
+        filter ??= new BraceletFilter();
+        
+        string sql = """
+                     SELECT DISTINCT item ->> @Lang AS material
+                     FROM bracelets,
+                          jsonb_array_elements(materials) AS item
+                     WHERE available = true 
+                       AND item ->> @Lang IS NOT NULL
+                     ORDER BY material;
+                     """;
+
+        return await DataProvider.QuerySingleAsync(async db =>
+        {
+            var result = await db.QueryAsync<string>(sql, new { Lang = filter.Lang });
+            Console.WriteLine(result.FirstOrDefault());
+            return result.ToList();
+        });
+    }
+
 }
 
 public record BraceletFilter(
